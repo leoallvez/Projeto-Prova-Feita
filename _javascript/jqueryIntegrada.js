d413b2fc9cac4dcd7fb09f7010a637bd9b34f3d4
leoallvez <leoallvez@hotmail.com>
2016-04-06 03:41:12 -0300

    commit

--- _javascript/jqueryIntegrada.js
+++ _javascript/jqueryIntegrada.js
@@ -2,25 +2,25 @@ $(document).ready(function(){
 
 	$('#calculate').click(function() {
 
-		var questions = $('#number-of-issues').val();
-		var hits = $('#amount-of-hits').val();
+		var questions = parseInt($('#number-of-issues').val(),10);
+		var hits = parseInt($('#amount-of-hits').val(),10);
 			
-		if((hits > 40) || (questions > 40) || (hits < 0) || (questions < 0)) {
+		if(((hits < 0) || (hits > 40)) || ((questions < 0) || (questions > 40))) {
 			$('#result').html('<span>Não é possivel fazer esse calculo os valores devem variar entre 0 e 40!<span>');
 			$('#m2').empty();
-		}else if(hits > questions){
+		}else if(questions < hits){
 			$('#result').html('<span>Quantidade de acertos maior que as de questões!<span>');
 			$('#m2').empty();
-		}else{
-			if((hits != "") && (questions != "")){
-				var noteByHits = 10 / questions;
+		//NaN (Not a number)
+		}else if(!isNaN(hits) && !isNaN(questions)){
 
-				var fullNote = noteByHits * hits;
-				var percentage = (noteByHits * hits) * 0.3;
-				//x.toFixed(2) faz o arredondamento da variavel.
-				$('#result').html("<span><span class='green'>Nota integral: </span> "+ fullNote.toFixed(1) +"</span>");
-				$('#m2').html("<span><span class='green'>30% na M2: </span>"+ percentage.toFixed(1) +"</span>");
-			}
+			var noteByHits = 10 / questions;
+			var fullNote = noteByHits * hits;
+			var percentage = (noteByHits * hits) * 0.3;
+			//x.toFixed(2) faz o arredondamento da variavel.
+
+			$('#result').html("<span><span class='green'>Nota integral: </span> "+ fullNote.toFixed(1) +"</span>");
+			$('#m2').html("<span><span class='green'>30% na M2: </span>"+ percentage.toFixed(1) +"</span>");
 		}
 	});
 
